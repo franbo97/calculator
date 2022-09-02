61b301a51ce10da4ca5fb2e7d5eb3ac3dec03c40
franbo97 <francescogiombo@libero.it>
2022-09-02 11:52:03 -0400

    add py file es 2

--- test_calculator.py
+++ test_calculator.py
@@ -2,6 +2,6 @@ import unittest
 from calculator import multiply
 class TestSomething(unittest.TestCase):
     def test_multiply(self):
-        self.assertEqual(6, multiply(2,3))
+        self.assertEqual(7, multiply(2,3))
 if __name__ == '__main__':
     unittest.main()
